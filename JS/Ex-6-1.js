c9e7b733fc4705a87dd4cfe53cd83f9e30a01eab
Kimi L <kimi.lefort.pro@gmail.com>
2023-09-12 16:57:24 +0200

    Exercices 6 start

---
+++ JS/Ex-6-1.js
@@ -0,0 +1,21 @@
+/* class Circle {
+    constructor (xPos, yPos, radius) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.radius = radius;
+    }
+    move (xOffset, yOffset) {
+        this.xPos = xOffset
+        this.yPos = yOffset
+        console.log('New position x: ' + this.xPos + ' y: ' + this.yPos);
+
+    }
+    get surface() {
+        return ("Surface is " + Math.PI * this.radius**2)
+    }
+}
+
+let x = new Circle("2", "3", "8")
+
+x.move("8", "7");
+console.log(x.surface); */
---
+++ JS/Ex-6-2.js
@@ -0,0 +1,11 @@
+class Rectangle {
+    constructor(topLeftXPos, topLeftYPos, width, length) {
+        this.topLeftXPos = topLeftXPos;
+        this.topLeftYPos = topLeftYPos;
+        this.width = width;
+        this.length = length;
+    }
+    collides(otherRectangle) {
+        this.otherRectangle =
+    }
+}
