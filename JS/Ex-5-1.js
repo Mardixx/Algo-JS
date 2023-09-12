e3181c83f9cf546a276d9e80436b1da672d9eff9
Kimi L <kimi.lefort.pro@gmail.com>
2023-09-12 14:41:19 +0200

    Objects

---
+++ JS/Ex-5-1.js
@@ -0,0 +1,20 @@
+/* function askTvSerie() {
+    let nom = prompt("Give me a name of a serie")
+    let année = prompt("Give me a year")
+    let number = prompt("Number of cast")
+    let newMembre = [];
+    for (let i = 0; i < number; i++) {
+        let membre = prompt("Give me name of member(s)")
+        newMembre.push (membre);
+    }
+    let myObject = {
+        seriename: nom,
+
+        prodname: année,
+
+        castname: newMembre
+    }
+    
+    return myObject;
+}
+let mySerie = askTvSerie() */
---
+++ JS/Ex-5-2.js
@@ -0,0 +1,29 @@
+/* let randomizeCast = (castname) => {
+    let newArr = [];
+     for (let i = 0; i < castname.length; i++) {
+        castMembers = castname[Math.floor(Math.random() * castname.length)]
+        newArr.push(castMembers)
+        
+     }
+     return newArr;
+}
+let randomized = randomizeCast(mySerie.castname)
+console.log(randomized); */
+
+
+
+
+/*     function randomizeCast(tvSerie) {
+    tvSerie = askTvSerie()
+    let tvSerieArr = tvSerie["Name of the cast members"];
+    console.log(tvSerieArr);
+    let newArr = [];
+
+    for (let i = 0; i < tvSerieArr.length; i++) {
+        random = Math.floor(Math.random() * tvSerieArr.length);
+        newArr.push(random)
+        const newMembre = tvSerieArr[random];
+        console.log(tvSerieArr);
+    }
+    console.log(rand);   
+} */
